0468a03269ea0f56059f1e7558109efb108ef655
witalo <vent.wivf@gmail.com>
2026-03-02 00:42:23 -0500

    cambios

--- prestoras/settings.py
+++ prestoras/settings.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = 'prestoras.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis' if os.environ.get('USE_POSTGIS') == 'True' else 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'prestora'),
-        'USER': os.environ.get('DB_USER', 'italo'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'italo2025.*/'),
+        'NAME': os.environ.get('DB_NAME', '*****'),
+        'USER': os.environ.get('DB_USER', '*****'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '*****'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
         'OPTIONS': {
